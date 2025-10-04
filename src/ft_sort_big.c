e2c61543a06cf5d90859a57432d5bc686e5d8d8e
diegomontanari <diegomontanari.imp@gmail.com>
2025-10-04 18:15:56 +0200

    Minor comment update on ft_sort_big.c

--- src/ft_sort_big.c
+++ src/ft_sort_big.c
@@ -92,7 +92,7 @@ t_stack	**ft_sort_a(t_stack **stack_a, t_stack **stack_b)
 // And finally it makes final sort in stack_a after
 // all values pushed to stack_b, sorted and pushed
 // back to stack_a. Because, even the stack_a is
-// sorted at the end, the minimum number have to 
+// sorted at the end, the minimum number has to be
 // at the top of the stack_a. So, it simply brings
 // the smallest number of the stack_a to the top.
 void	ft_sort(t_stack **stack_a)
