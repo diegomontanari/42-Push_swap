80c4132eceade3e02a47abf4713080bce67b1040
Diego Montanari <dmontana@e3r3p4.42roma.it>
2025-10-04 18:30:42 +0200

    Fixed norminette 25 lines max ft

--- src/input_parser.c
+++ src/input_parser.c
@@ -84,33 +84,39 @@ t_stack	*ft_process_quoted_numbers(char **argv)
 // if (!*str) manages things like: " " or "" etc...
 // - Converts the string to a number, stopping at invalid characters.
 // - If the string is invalid or the number overflows, triggers an error.
-int	ft_atoi2(const char *str)
+static const char	*ft_skip_whitespace_and_sign(const char *str, int *sign)
 {
-	int				mod;
-	long long int	i;
-
-	i = 0;
-	mod = 1;
-	while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\f'
-		|| *str == '\v' || *str == '\r')
+	*sign = 1;
+	while (*str == ' ' || *str == '\t' || *str == '\n'
+		|| *str == '\f' || *str == '\v' || *str == '\r')
 		str++;
 	if (*str == '-')
 	{
-		mod = -1;
+		*sign = -1;
 		str++;
 	}
 	else if (*str == '+')
 		str++;
-	if (!str || !ft_isdigit(*str))
+	if (!*str || !ft_isdigit(*str))
 		ft_error();
+	return (str);
+}
+
+int	ft_atoi2(const char *str)
+{
+	int				sign;
+	long long int	result;
+
+	result = 0;
+	str = ft_skip_whitespace_and_sign(str, &sign);
 	while (*str)
 	{
 		if (!ft_isdigit(*str))
 			ft_error();
-		i = i * 10 + (*str - 48);
+		result = result * 10 + (*str - '0');
 		str++;
 	}
-	if ((mod * i) > 2147483647 || (mod * i) < -2147483648)
+	if ((sign * result) > 2147483647 || (sign * result) < -2147483648)
 		ft_error();
-	return (mod * i);
+	return ((int)(sign * result));
 }
